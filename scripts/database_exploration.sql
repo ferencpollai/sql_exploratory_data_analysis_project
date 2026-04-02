210408060b1172e1016370341ca0f3533e454e68
ferencpollai <57192039+ferencpollai@users.noreply.github.com>
2026-04-02 14:08:08 +0300

    Update database_exploration.sql

--- scripts/database_exploration.sql
+++ scripts/database_exploration.sql
@@ -1,4 +1,14 @@
--- Explore All Object in the Database
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Script Purpose:
+    I used this script just to get to know the database better. To see what's
+    inside the tables and columns.
+===============================================================================
+*/
+
+-- Explore All Objects in the Database
 
 SELECT * FROM INFORMATION_SCHEMA.TABLES
 
